92aa115336afb03c3316261114794f22f2df87bb
Your Actual Name <stan.amsellem@gmail.com>
2016-03-07 18:33:31 +0100

    initial commit and setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: 'mpb.jsx',
+  output: {
+    path: "./",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
